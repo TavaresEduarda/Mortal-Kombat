123557d128d476e2ca99f8e35a867ebe70d32dd9
TavaresEduarda <eduardaferreiratc@gmail.com>
2026-02-19 11:11:56 -0300

    refactor: rename functions and variables to English

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 
-function lista(){
-    let name = document.getElementById("name").value
-    let power = document.getElementById("power").value
-    let realm = document.getElementById("realm").value  
-    let li = document.createElement("li")
-
-    li.innerHTML = `<span>Name: ${name}</span>
-    <button class="excluir" onclick="deletar(this)">Excluir</button>`
-    li.innerHTML += `<p>Power: ${power}</p>`
-    li.innerHTML += `<p>Realm: ${realm}</p>`
-    document.getElementById("lista").appendChild(li)
-    document.getElementById("name").value = ""  
-    document.getElementById("power").value = ""
-    document.getElementById("realm").value = "" 
-
-
+function addToList() {
+    let name = document.getElementById("name").value;
+    let power = document.getElementById("power").value;
+    let realm = document.getElementById("realm").value;
     
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+        <span>Name: ${name}</span>
+        <button class="delete" onclick="deleteItem(this)">Delete</button>
+        <p>Power: ${power}</p>
+        <p>Realm: ${realm}</p>
+    `;
+
+    document.getElementById("list").appendChild(li);
+
+    document.getElementById("name").value = "";
+    document.getElementById("power").value = "";
+    document.getElementById("realm").value = "";
 }
 
-function deletar(li){
-    li.parentElement.remove()
-
+function deleteItem(button) {
+    button.parentElement.remove();
 }
